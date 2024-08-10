0abbbe282aa6925a43a38adb6cb90abb1f9e770a
mav <michael@studiomav.com>
2024-08-09 18:28:54 -0700

    Fix deck.gd indentation

--- example_solitaire/scripts/deck.gd
+++ example_solitaire/scripts/deck.gd
@@ -2,16 +2,16 @@ class_name Deck
 extends CardCollection3D
 
 func can_insert_card(_card: FaceCard3D, _from_collection) -> bool:
-  return false
+	return false
 
 
 func can_reorder_card(_card: FaceCard3D) -> bool:
-  return false
+	return false
 
 
 func can_select_card(_card: FaceCard3D) -> bool:
-  return true
+	return true
 
 
 func can_remove_card(_card: FaceCard3D) -> bool:
-  return false
+	return false
