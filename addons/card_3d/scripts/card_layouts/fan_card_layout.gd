e3091daa058d3a1935b5f0ae2160408e2c2f5ea7
Tyson Decker <tyson.decker@hq.bill.com>
2025-06-11 13:54:38 -0600

    issue-8 add fan direction exported variable to the fan card layout and support "reverse"

--- addons/card_3d/scripts/card_layouts/fan_card_layout.gd
+++ addons/card_3d/scripts/card_layouts/fan_card_layout.gd
@@ -1,6 +1,12 @@
 class_name FanCardLayout
 extends CardLayout
 
+enum FanDirection {
+	NORMAL, REVERSE
+}
+
+@export var direction: FanDirection = FanDirection.NORMAL
+
 var arc_angle_deg: float = 90.0:
 	set(a):
 		arc_angle_deg = a
@@ -19,7 +25,7 @@ func calculate_card_positions(num_cards: int) -> Array[Vector3]:
 		var angle = start_angle - (i * angle_step)
 		var x = arc_radius * cos(angle)
 		var y = (arc_radius * sin(angle)) - arc_radius
-		var position = Vector3(x, y, 0.001 * i)
+		var position = _get_arc_position(x, y, i)
 		positions.append(position)
 		
 	return positions
@@ -31,7 +37,7 @@ func calculate_card_position_by_index(num_cards: int, index: int) -> Vector3:
 	var angle = start_angle - ((index + 1) * angle_step)
 	var x = arc_radius * cos(angle)
 	var y = (arc_radius * sin(angle)) - arc_radius
-	var position = Vector3(x, y, 0.001 * (index + 1))
+	var position = _get_arc_position(x, y, index)
 	
 	return position
 
@@ -42,7 +48,7 @@ func calculate_card_rotations(num_cards: int) -> Array[Vector3]:
 	
 	for i in range(1, num_cards + 1):
 		var angle = start_angle - (i * angle_step)
-		var rotation_quat = Quaternion(Vector3(0, 0, 1), angle - PI/2)
+		var rotation_quat = _get_rotation_quat(angle)
 		rotations.append(rotation_quat.get_euler())
 	
 	return rotations
@@ -51,5 +57,19 @@ func calculate_card_rotations(num_cards: int) -> Array[Vector3]:
 func calculate_card_rotation_by_index(num_cards: int, index: int) -> Vector3:
 	var angle_step = deg_to_rad(arc_angle_deg) / (num_cards + 1)
 	var angle = start_angle - ((index + 1) * angle_step)
-	var rotation_quat = Quaternion(Vector3(0, 0, 1), angle - PI/2)
+	var rotation_quat = _get_rotation_quat(angle)
 	return rotation_quat.get_euler()
+
+
+func _get_arc_position(x, y, i) -> Vector3:
+	if direction == FanDirection.NORMAL:
+		return Vector3(x, y, 0.001 * i)
+	else:
+		return Vector3(x, -y, 0.001 * i)
+
+
+func _get_rotation_quat(angle) -> Quaternion:
+	if direction == FanDirection.NORMAL:
+		return Quaternion(Vector3(0, 0, 1), angle - PI/2)
+	else:
+		return Quaternion(Vector3(0, 0, 1), -angle + PI/2)
