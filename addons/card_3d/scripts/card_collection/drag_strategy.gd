dd0c7a6058a13a3b4c4de5e3ee0b000a1144aebb
Tyson Decker <tyson.decker@gmail.com>
2025-08-21 18:02:02 -0600

    added drag strategy and start refactoring examples to use it

---
+++ addons/card_3d/scripts/card_collection/drag_strategy.gd
@@ -0,0 +1,41 @@
+class_name DragStrategy
+extends Resource
+"""
+DragStrategy
+======================
+
+This module defines an interface that describes how cards can drag from different collections.
+It can be configured either by:
+	1. setting the drag stragegy on your collection to "NewDragStrategy" via the resource menu in 
+		node the inspector and changing the default boolean values
+	2. creating a custom script that extends DragStrategy and overriding 
+		(can_select_card, can_remove_card, can_reorder_card, can_insert_card)
+		to describe checks conditional to the card and collection
+"""
+
+
+@export_group("Default Behavior")
+@export var can_select: bool = true
+@export var can_remove: bool = true
+@export var can_reorder: bool = true
+@export var can_insert: bool = true
+
+
+func can_select_card(_card, to_collection: CardCollection3D) -> bool:
+	return can_select
+
+
+func can_remove_card(_card, _to_collection: CardCollection3D) -> bool:
+	return can_remove
+
+
+func can_reorder_card(_card, _to_collection: CardCollection3D) -> bool:
+	return can_reorder
+
+
+func can_insert_card(
+	_card,
+	_to_collection: CardCollection3D,
+	_from_collection: CardCollection3D
+	) -> bool:
+	return can_insert
--- example_solitaire/scripts/card_column.gd
+++ example_solitaire/scripts/card_column.gd
@@ -1,13 +1,15 @@
-class_name CardColumn
-extends CardCollection3D
+class_name CardColumnDragStrategy
+extends DragStrategy
 
-func can_insert_card(card: FaceCard3D, _from_collection) -> bool:
+func can_insert_card(card: FaceCard3D, to_collection, _from_collection) -> bool:
+	var cards = to_collection.cards
+	
 	if cards.size() == 0:
 		return true
 
 	var last_card = cards[cards.size() - 1]
 	var is_prior_rank = card.rank == last_card.rank - 1
-	var is_opposite_color = false;
+	var is_opposite_color = false
 
 	if (card.suit == FaceCards.Suit.HEART or card.suit == FaceCards.Suit.DIAMOND) and \
 			(last_card.suit == FaceCards.Suit.CLUB or last_card.suit == FaceCards.Suit.SPADE):
@@ -19,11 +21,11 @@ func can_insert_card(card: FaceCard3D, _from_collection) -> bool:
 	return last_card.face_down or (is_opposite_color and is_prior_rank)
 
 
-func can_reorder_card(_card: FaceCard3D) -> bool:
+func can_reorder_card(_card: FaceCard3D, collection: CardCollection3D) -> bool:
 	return false
 
 
-func can_select_card(card: FaceCard3D) -> bool:
-	var card_index = card_indicies[card]
+func can_select_card(card: FaceCard3D, collection: CardCollection3D) -> bool:
+	var card_index = collection.card_indicies[card]
 
-	return !card.face_down or card_index == (cards.size() - 1)
+	return !card.face_down or card_index == (collection.cards.size() - 1)
