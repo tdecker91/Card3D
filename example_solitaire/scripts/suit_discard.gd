b58a275a1b2dfac3ad8e0eb0ab5ce51b95194dae
Tyson Decker <tyson.decker@gmail.com>
2025-08-21 18:29:07 -0600

    finish fixing solitaire example to use drag strategy

--- example_solitaire/scripts/suit_discard.gd
+++ example_solitaire/scripts/suit_discard.gd
@@ -1,27 +1,15 @@
 class_name SuitDiscard
-extends CardCollection3D
+extends DragStrategy
 
-func can_insert_card(card: FaceCard3D, from_collection: CardCollection3D) -> bool:
+func can_insert_card(card: FaceCard3D, to_collection: CardCollection3D, from_collection: CardCollection3D) -> bool:
 	if from_collection.card_indicies.has(card) and from_collection.card_indicies[card] < from_collection.cards.size() - 1:
 		return false
 
-	if cards.size() == 0:
+	if to_collection.cards.size() == 0:
 		return card.rank == FaceCards.Rank.ACE
 
-	var last_card = cards[cards.size() - 1]
+	var last_card = to_collection.cards[to_collection.cards.size() - 1]
 	var is_next_rank = (card.rank == last_card.rank + 1) or (last_card.rank == FaceCards.Rank.ACE and card.rank == FaceCards.Rank.TWO)
 	var is_same_suit = last_card.suit == card.suit
 
 	return is_next_rank and is_same_suit
-
-
-func can_reorder_card(_card: FaceCard3D) -> bool:
-	return false
-
-
-func can_remove_card(_card: FaceCard3D) -> bool:
-	return false
-
-
-func can_select_card(_card: FaceCard3D) -> bool:
-	return false
