642b113f39fce39931477468cb2195f62ba31a25
camperotactico <contact@camperotacti.co>
2025-10-07 18:28:31 +0200

    Refactor drag_controller to make use of card_deselect instead of reading mouse button inputs.

--- addons/card_3d/scripts/drag_controller.gd
+++ addons/card_3d/scripts/drag_controller.gd
@@ -23,32 +23,44 @@ signal card_moved(card, from_collection, to_collection, from_index, to_index)
 @export var max_drag_y_rotation_deg: int = 65
 @export var max_drag_x_rotation_deg: int = 65
 
-
 @export var card_drag_plane = Plane(Vector3(0, 0, 1), 1.5): # plane card is moved across on drag
 	set(plane):
 		card_drag_plane = plane
+
+## The minimum distance the mouse needs to travel in viewport coordinates to consider the input a drag.
+@export_range(1,100.0,1,"suffix:px")  var card_drag_threshold: float = 10.0:
+	set(value):
+		_set_card_drag_threshold_squared(value)
 
 var _camera: Camera3D # camera used for determining where mouse is on drag plane
 var _dragging_card: Card3D # card that is being dragged
 var _drag_from_collection: CardCollection3D # collection card being dragged from
 var _dragging: bool = false
+var _card_drag_threshold_squared: float
+var _selection_start_mouse_position: Vector2
+var _current_mouse_position: Vector2
 var _hovered_collection: CardCollection3D # collection about to drop card into
 var _hovered_collection_plane: Plane
 var _hovered_collection_layout_direction: Vector3
 var _card_collections: Array[CardCollection3D] = []
 
+func _set_card_drag_threshold_squared(threshold_value: float) -> void:
+	_card_drag_threshold_squared = threshold_value*threshold_value
 
 func _ready():
 	var window = get_window()
 	_camera = window.get_camera_3d()
-
+	
 	for child in get_children():
 		if child is CardCollection3D:
 			add_card_collection(child)
+			
+	_set_card_drag_threshold_squared(card_drag_threshold)
 
 func add_card_collection(card_collection: CardCollection3D) -> void:
 	_card_collections.append(card_collection)
 	card_collection.card_selected.connect(_on_collection_card_selected.bind(card_collection))
+	card_collection.card_deselected.connect(_on_collection_card_deselected.bind(card_collection))
 	card_collection.mouse_enter_drop_zone.connect(_on_collection_mouse_enter_drop_zone.bind(card_collection))
 	card_collection.mouse_exit_drop_zone.connect(_on_collection_mouse_exit_drop_zone.bind(card_collection))
 
@@ -56,23 +68,40 @@ func remove_card_collection(card_collection: CardCollection3D) -> void:
 	if _card_collections.has(card_collection):
 		_card_collections.erase(card_collection)
 		card_collection.card_selected.disconnect(_on_collection_card_selected.bind(card_collection))
+		card_collection.card_deselected.disconnect(_on_collection_card_deselected.bind(card_collection))
 		card_collection.mouse_enter_drop_zone.disconnect(_on_collection_mouse_enter_drop_zone.bind(card_collection))
 		card_collection.mouse_exit_drop_zone.disconnect(_on_collection_mouse_exit_drop_zone.bind(card_collection))
 
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if _dragging and event.button_index == 1 and !event.pressed:
-			var m: Vector2 = get_viewport().get_mouse_position()
-			_stop_drag(m)
-	elif event is InputEventMouseMotion:
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_current_mouse_position = get_viewport().get_mouse_position()
 		if _dragging:
-			_handle_drag_event(event)
+			_handle_drag_event()
+		elif _dragging_card:
+			if _selection_start_mouse_position.distance_squared_to(_current_mouse_position) > _card_drag_threshold_squared:
+				_drag_card_start()
 
 
 func _on_collection_card_selected(card: Card3D, collection: CardCollection3D):
-	_drag_card_start(card, collection)
+	_selection_start_mouse_position = get_viewport().get_mouse_position()
+	_drag_from_collection = collection
+	_dragging_card = card
 
+	for card_collection in _card_collections:
+		card_collection.hover_disabled = true
+	
+func _on_collection_card_deselected(card: Card3D, _collection: CardCollection3D):
+	if _dragging_card != card:
+		return
+
+	if _dragging:
+		_stop_drag()
+	else:
+		_dragging_card = null
+		_drag_from_collection = null
+		for card_collection in _card_collections:
+			card_collection.hover_disabled = false
 
 func _on_collection_mouse_enter_drop_zone(collection: CardCollection3D):
 	_set_hovered_collection(collection)
@@ -136,13 +165,10 @@ func _drop_card_to_another_collection(mouse_position: Vector2):
 	c.global_position = card_global_position
 
 
-func _drag_card_start(card: Card3D, drag_from_collection: CardCollection3D):
+func _drag_card_start():
 	_dragging = true
-	_drag_from_collection = drag_from_collection
-	_dragging_card = card
 	_dragging_card.disable_collision()
 	_dragging_card.remove_hovered()
-
 	_drag_from_collection.enable_drop_zone()
 
 	for collection in _card_collections:
@@ -151,21 +177,21 @@ func _drag_card_start(card: Card3D, drag_from_collection: CardCollection3D):
 
 		collection.hover_disabled = true
 
-	drag_started.emit(card)
+	drag_started.emit(_dragging_card)
 
 
-func _stop_drag(mouse_position: Vector2):
+func _stop_drag():
 	var can_insert: bool = true
 
 	if _hovered_collection != null:
 		can_insert = _hovered_collection.can_insert_card(_dragging_card, _drag_from_collection) and _drag_from_collection.can_remove_card(_dragging_card)
 
 	if not can_insert:
-		_return_card_to_collection(mouse_position)
+		_return_card_to_collection(_current_mouse_position)
 	elif _hovered_collection == null or _hovered_collection == _drag_from_collection:
-		_return_card_to_collection(mouse_position)
+		_return_card_to_collection(_current_mouse_position)
 	elif _hovered_collection != null and _hovered_collection != _drag_from_collection:
-		_drop_card_to_another_collection(mouse_position)
+		_drop_card_to_another_collection(_current_mouse_position)
 
 	_drag_from_collection.disable_drop_zone()
 	_dragging_card.enable_collision()
@@ -184,9 +210,8 @@ func _stop_drag(mouse_position: Vector2):
 
 	drag_stopped.emit(card)
 
-func _handle_drag_event(_event: InputEventMouseMotion):
-	var m: Vector2 = get_viewport().get_mouse_position()
-	var position_3d = card_drag_plane.intersects_ray(_camera.project_ray_origin(m), _camera.project_ray_normal(m))
+func _handle_drag_event():
+	var position_3d = card_drag_plane.intersects_ray(_camera.project_ray_origin(_current_mouse_position), _camera.project_ray_normal(_current_mouse_position))
 	var card_position = _dragging_card.global_position
 
 	var x_distance = position_3d.x - card_position.x
@@ -218,7 +243,7 @@ func _handle_drag_event(_event: InputEventMouseMotion):
 	_dragging_card.global_position.z = position_3d.z
 
 	if _hovered_collection != null and position_3d != null and _hovered_collection.can_reorder_card(_dragging_card):
-		var index = _get_hovered_collection_index_at_mouse_pos(m)
+		var index = _get_hovered_collection_index_at_mouse_pos(_current_mouse_position)
 		_hovered_collection.preview_card_drop(_dragging_card, index)
 
 
